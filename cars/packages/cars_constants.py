713b6ed9c9859a1f962d7e9e251635a34185e774
GerritGeens <geens.gerrit@gmail.com>
2024-05-25 13:13:04 +0200

    haas

---
+++ cars/packages/cars_constants.py
@@ -0,0 +1,15 @@
+MAKE = "Volvo"
+PLOTTING_COLORS = ['pink', 'green', 'blue', 'magenta', 'black', 'purple', 'rainbow', 'crazy_color']
+
+
+fuel_dict = {
+    'G': 'Gasoline', 
+    'Gas': 'Gasoline',
+    'Gasoline Fuel': 'Gasoline',
+    'Gasoline/Mild Electric Hybrid': 'Hybrid',
+    'Hybrid Fuel' : 'Hybrid',
+    'Plug-In Electric/Gas': 'Hybrid',
+    'Plug-In Hybrid' : 'Hybrid', 
+    'Premium Unleaded': 'Gasoline',
+    'Regular Unleaded': 'Gasoline'
+}
--- cars/packages/cars_functies.py
+++ cars/packages/cars_functies.py
@@ -1,27 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 
-def read_cars_data (cars_data_data_file_path): 
+def read_cars_data(cars_data_data_file_path): 
 
     # Read data the CSV file into a DataFrame
     df = pd.read_csv(cars_data_data_file_path)
 
     return df
-
-
-
-def vertaal_letter_in_kleur (letter) :
-    """
-    translate a an input letter to an RGB channel 
-    """
-    letter_naar_channel = {
-        'R': 0,
-        'G': 1,
-        'B': 2,
-        'r': 0,
-        'g': 1,
-        'b': 2
-    }
-
-    return letter_naar_channel[letter]
 
